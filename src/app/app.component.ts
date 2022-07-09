9af33110d1669671f958d97ad5c2cb81eb855909
David Valentine <davidvalentine314@gmail.com>
2022-07-09 00:57:04 -0600

    additional comments on behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,13 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class AppComponent {
 
   selectedNumbers:number=0;
-  showDialog:boolean=false;
   @ViewChild('ModalReference') modalReference!:ElementRef;
   numbersChanged(){
 
   }
 
   toggleDialog(){
-    //this.showDialog = !this.showDialog;
+    //this.showDialog = !this.showDialog; // this works but makes it easier to click off the modal and dismiss it. 
     if(!this.modalReference.nativeElement.open){
       this.modalReference.nativeElement.showModal();
     }else{
